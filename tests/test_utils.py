5ee82b15fe5b80ada431e9fdb4f1807af18e16be
Fabien MARTY <fabien.marty@gmail.com>
2014-08-24 14:58:33 +0200

    flake8 fixes

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -15,22 +15,22 @@ class UtilsTestCase(tornado.testing.AsyncTestCase):
     def test_protocol2(self):
         res = format_args_in_redis_protocol("SET", "key", "foobar")
         self.assertEquals(res, b"*3\r\n$3\r\nSET\r\n$3\r\nkey\r\n"
-                                "$6\r\nfoobar\r\n")
+                          b"$6\r\nfoobar\r\n")
         res = format_args_in_redis_protocol("SET", "key", u"foobar")
         self.assertEquals(res, b"*3\r\n$3\r\nSET\r\n$3\r\nkey\r\n"
-                                "$6\r\nfoobar\r\n")
+                          b"$6\r\nfoobar\r\n")
 
     def test_protocol3(self):
         res = format_args_in_redis_protocol("SET", "key", u"é")
         self.assertEquals(res, b"*3\r\n$3\r\nSET\r\n$3\r\nkey\r\n"
-                                "$2\r\n\xc3\xa9\r\n")
+                          b"$2\r\n\xc3\xa9\r\n")
 
     def test_protocol4(self):
         res = format_args_in_redis_protocol("SET", "key", b"\000")
         self.assertEquals(res, b"*3\r\n$3\r\nSET\r\n$3\r\nkey\r\n"
-                                "$1\r\n\000\r\n")
+                          b"$1\r\n\000\r\n")
 
     def test_protocol5(self):
         res = format_args_in_redis_protocol("SET", "key", 1)
         self.assertEquals(res, b"*3\r\n$3\r\nSET\r\n$3\r\nkey\r\n"
-                                "$1\r\n1\r\n")
+                          b"$1\r\n1\r\n")
