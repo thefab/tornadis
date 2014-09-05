dcc063326321f6bc9384d68e32f66d431c68faf3
Fabien MARTY <fabien.marty@gmail.com>
2014-09-05 14:04:42 +0200

    restrict namespace

--- tornadis/__init__.py
+++ tornadis/__init__.py
@@ -6,3 +6,8 @@
 
 version_info = (0, 0, '1')
 __version__ = ".".join([str(x) for x in version_info])
+
+from tornadis.client import Client
+from tornadis.pool import ClientPool
+
+__all__ = ['Client', 'ClientPool']
