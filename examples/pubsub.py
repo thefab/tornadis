2713b6372cabdeca13c37823844b95d20720b9e8
Fabien MARTY <fabien.marty@gmail.com>
2014-09-08 11:42:44 +0200

    add an example for pubsub

---
+++ examples/pubsub.py
@@ -0,0 +1,24 @@
+import tornado
+import tornadis
+
+
+@tornado.gen.coroutine
+def pubsub():
+    client = tornadis.Client()
+    yield client.connect()
+    reply = yield client.call("PSUBSCRIBE", "*")
+    print reply
+    while True:
+        reply = yield client.pop_message()
+        print reply
+
+
+def stop_loop(future):
+    loop = tornado.ioloop.IOLoop.instance()
+    loop.stop()
+
+
+loop = tornado.ioloop.IOLoop.instance()
+future = pubsub()
+loop.add_future(future, stop_loop)
+loop.start()
