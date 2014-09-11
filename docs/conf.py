6c93cfc586746957b84766c89b243532a0d57c8c
Fabien MARTY <fabien.marty@gmail.com>
2014-09-11 07:45:22 +0200

    better output

--- docs/conf.py
+++ docs/conf.py
@@ -262,3 +262,6 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+napoleon_use_ivar = True
+napoleon_use_param = False
