4cef55edf528d09b3a62d7d45c87d7d401c6a9ce
Fabien MARTY <fabien.marty@gmail.com>
2014-09-12 08:00:00 +0200

    docs

--- tornadis/connection.py
+++ tornadis/connection.py
@@ -8,12 +8,24 @@ import socket
 import tornado.iostream
 import tornado.gen
 
-# FIXME: error handling
-
 
 class Connection(object):
+    """Low level connection object.
+
+    Attributes:
+        host (string): the host name to connect to
+        port (int): the port to connect to
+        connected (boolean): is the connection object really connected
+    """
 
     def __init__(self, host='localhost', port=6379, ioloop=None):
+        """Constructor.
+
+        Args:
+            host (string): the host name to connect to
+            port (int): the port to connect to
+            ioloop (IOLoop): the tornado ioloop to use
+        """
         self.host = host
         self.port = port
         self.connected = False
@@ -22,6 +34,11 @@ class Connection(object):
 
     @tornado.gen.coroutine
     def connect(self):
+        """Connects the object to the host:port.
+
+        Returns:
+            Future: a Future object
+        """
         self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.__stream = tornado.iostream.IOStream(self.__socket,
                                                   io_loop=self.__ioloop)
@@ -29,13 +46,32 @@ class Connection(object):
         self.connected = True
 
     def disconnect(self):
+        """Disconnects the object.
+        """
         self.__stream.close()
         self.connected = False
 
     def write(self, data):
+        """Writes some data to the host:port
+
+        Args:
+            data (str): string (buffer) to write to the host:port
+
+        Returns:
+            Future: a Future object "resolved" when the data is written
+                on the socket
+        """
         return self.__stream.write(data)
 
     def register_read_until_close_callback(self, callback=None,
                                            streaming_callback=None):
+        """Registers a callback called when data are available on the socket.
+
+        Args:
+            callback (callable): callback to call when the connection is
+                closed
+            streaming_callback (callable): callback to call when data are
+                available on the socket
+        """
         self.__stream.read_until_close(callback=callback,
                                        streaming_callback=streaming_callback)
