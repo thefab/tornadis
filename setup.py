8fe02684972c6debc19c974ec5c3c75423b88a49
Fabien MARTY <fabien.marty@gmail.com>
2016-09-03 10:21:21 +0200

    Revert "remove python 3.2 support"
    This reverts commit e9310d2abe85fc39c41f407bc65277135f04854a.

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ setup(
         'Operating System :: POSIX',
         'Operating System :: Unix',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
